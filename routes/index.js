9a8f9b979b459010dc5695b6901c315efb8fb60e
aymanelkadi <aymannedaa@gmail.com>
2020-04-13 18:20:00 +0300

    Fix linting issues
    After adding eslinter rules, fix linting issues accordingly

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Demo server' });
 });
 
-router.get('/demo', function(req, res, next) {
+router.get('/demo', (req, res) => {
   res.send("I'm alive!");
 });
 
