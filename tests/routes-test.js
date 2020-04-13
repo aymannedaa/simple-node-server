2f0f7d6c900e1354ee7f55340a78a4eaa0168ca8
aymanelkadi <aymannedaa@gmail.com>
2020-04-13 16:06:08 +0300

    Add unit tests
    - Add unit tests for testing server API
    - Add respective dev dependencies to package.json
    - Allow running tests by using 'npm test'

---
+++ tests/routes-test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const expect = require('chai').expect;
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+chai.use(chaiHttp);
+chai.should();
+
+describe("Test webserver API routes", () => {
+        it("GET /demo should get I'm alive message", (done) => {
+             chai.request(app)
+                 .get('/demo')
+                 .end((err, res) => {
+                     res.should.have.status(200);
+                     res.text.should.equal('I\'m alive!');
+                     done();
+                  });
+         });
+         it("GET / should return 200 status", (done) => {
+            chai.request(app)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                 });
+        });
+        it("GET invalid route should return 404 status", (done) => {
+            chai.request(app)
+                .get('/INVALID_ROUTE')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    expect(res.text).to.not.be.empty;
+                    done();
+                 });
+        });
+});
