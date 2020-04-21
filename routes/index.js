cbe7c05f088891d00e441407149f83b682001e05
aymanelkadi <aymannedaa@gmail.com>
2020-04-21 08:13:23 +0300

    Show random number on root page
    Generate random number when server starts to identify the server

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const number = Math.ceil(Math.random() * 100000);
+
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: `Demo server version ${process.env.npm_package_version}` });
+  res.render('index', { title: `Demo server version ${process.env.npm_package_version}`, random: number });
 });
 
 router.get('/demo', (req, res) => {
